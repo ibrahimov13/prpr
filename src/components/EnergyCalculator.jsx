e4945ab8e58e378513595c96e10d545460ae9465
Sayyad Ibrahimov <sayyadibrahimov01@gmail.com>
2024-03-07 01:19:20 +0400

    asdsd

--- src/components/EnergyCalculator.jsx
+++ src/components/EnergyCalculator.jsx
@@ -1,254 +1,247 @@
 import React, { useState, useEffect } from 'react';
 
-const energyTypes = [
+const energyenerjiTypes = [
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Taşkömürü',
-    Katsayi: 0.61,
+    unit: 'ton',
+    'Power Supply': 'Taşkömürü',
+    fixed: 0.61,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Kok Kömürü',
-    Katsayi: 0.72,
+    unit: 'ton',
+    'Power Supply': 'Kok Kömürü',
+    fixed: 0.72,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Briket',
-    Katsayi: 0.5,
+    unit: 'ton',
+    'Power Supply': 'Briket',
+    fixed: 0.5,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Linyit teshin ve sanayi',
-    Katsayi: 0.3,
+    unit: 'ton',
+    'Power Supply': 'Linyit teshin ve sanayi',
+    fixed: 0.3,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Linyit santral',
-    Katsayi: 0.2,
+    unit: 'ton',
+    'Power Supply': 'Linyit santral',
+    fixed: 0.2,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Elbistan Linyiti',
-    Katsayi: 0.11,
+    unit: 'ton',
+    'Power Supply': 'Elbistan Linyiti',
+    fixed: 0.11,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Petrokok',
-    Katsayi: 0.76,
+    unit: 'ton',
+    'Power Supply': 'Petrokok',
+    fixed: 0.76,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Prina',
-    Katsayi: 0.43,
+    unit: 'ton',
+    'Power Supply': 'Prina',
+    fixed: 0.43,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Talaş',
-    Katsayi: 0.3,
+    unit: 'ton',
+    'Power Supply': 'Talaş',
+    fixed: 0.3,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Kabuk',
-    Katsayi: 0.225,
+    unit: 'ton',
+    'Power Supply': 'Kabuk',
+    fixed: 0.225,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Grafit',
-    Katsayi: 0.8,
+    unit: 'ton',
+    'Power Supply': 'Grafit',
+    fixed: 0.8,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Kok tozu',
-    Katsayi: 0.6,
+    unit: 'ton',
+    'Power Supply': 'Kok tozu',
+    fixed: 0.6,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Maden',
-    Katsayi: 0.55,
+    unit: 'ton',
+    'Power Supply': 'Maden',
+    fixed: 0.55,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Elbistan Linyiti',
-    Katsayi: 0.11,
+    unit: 'ton',
+    'Power Supply': 'Elbistan Linyiti',
+    fixed: 0.11,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Asfaltit',
-    Katsayi: 0.43,
+    unit: 'ton',
+    'Power Supply': 'Asfaltit',
+    fixed: 0.43,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Odun',
-    Katsayi: 0.3,
+    unit: 'ton',
+    'Power Supply': 'Odun',
+    fixed: 0.3,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Hayvan ve Bitki Artığı',
-    Katsayi: 0.23,
+    unit: 'ton',
+    'Power Supply': 'Hayvan ve Bitki Artığı',
+    fixed: 0.23,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Ham Petrol',
-    Katsayi: 1.05,
+    unit: 'ton',
+    'Power Supply': 'Ham Petrol',
+    fixed: 1.05,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Fuel Oil No: 4',
-    Katsayi: 0.96,
+    unit: 'ton',
+    'Power Supply': 'Fuel Oil No: 4',
+    fixed: 0.96,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Fuel Oil No: 5',
-    Katsayi: 1.003,
+    unit: 'ton',
+    'Power Supply': 'Fuel Oil No: 5',
+    fixed: 1.003,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Fuel Oil No: 6',
-    Katsayi: 0.986,
+    unit: 'ton',
+    'Power Supply': 'Fuel Oil No: 6',
+    fixed: 0.986,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Motorin',
-    Katsayi: 1.02,
+    unit: 'ton',
+    'Power Supply': 'Motorin',
+    fixed: 1.02,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Benzin',
-    Katsayi: 1.04,
+    unit: 'ton',
+    'Power Supply': 'Benzin',
+    fixed: 1.04,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Gazyağı',
-    Katsayi: 0.829,
+    unit: 'ton',
+    'Power Supply': 'Gazyağı',
+    fixed: 0.829,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Siyah Likör',
-    Katsayi: 0.3,
+    unit: 'ton',
+    'Power Supply': 'Siyah Likör',
+    fixed: 0.3,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Nafta',
-    Katsayi: 1.04,
+    unit: 'ton',
+    'Power Supply': 'Nafta',
+    fixed: 1.04,
   },
   {
-    Birim: 'm3',
-    'Enerji Kaynağı': 'Doğal Gaz',
-    Katsayi: 0.825,
+    unit: 'm3',
+    'Power Supply': 'Doğal Gaz',
+    fixed: 0.825,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Kok Gazı',
-    Katsayi: 0.82,
+    unit: 'ton',
+    'Power Supply': 'Kok Gazı',
+    fixed: 0.82,
   },
   {
-    Birim: 'm3',
-    'Enerji Kaynağı': 'Kok Gazı',
-    Katsayi: 0.403,
+    unit: 'm3',
+    'Power Supply': 'Kok Gazı',
+    fixed: 0.403,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'Yüksek Fırın Gazı ',
-    Katsayi: 0.054,
+    unit: 'ton',
+    'Power Supply': 'Yüksek Fırın Gazı ',
+    fixed: 0.054,
   },
   {
-    Birim: 'm3',
-    'Enerji Kaynağı': 'Yüksek Fırın Gazı',
-    Katsayi: 0.069,
+    unit: 'm3',
+    'Power Supply': 'Yüksek Fırın Gazı',
+    fixed: 0.069,
   },
   {
-    Birim: 'm3',
-    'Enerji Kaynağı': 'Çelikhane Gazı',
-    Katsayi: 0.15,
+    unit: 'm3',
+    'Power Supply': 'Çelikhane Gazı',
+    fixed: 0.15,
   },
   {
-    Birim: 'm3',
-    'Enerji Kaynağı': 'Rafineri Gazı',
-    Katsayi: 0.878,
+    unit: 'm3',
+    'Power Supply': 'Rafineri Gazı',
+    fixed: 0.878,
   },
   {
-    Birim: 'm3',
-    'Enerji Kaynağı': 'Asetilen',
-    Katsayi: 1.423,
+    unit: 'm3',
+    'Power Supply': 'Asetilen',
+    fixed: 1.423,
   },
   {
-    Birim: 'm3',
-    'Enerji Kaynağı': 'Propan',
-    Katsayi: 1.02,
+    unit: 'm3',
+    'Power Supply': 'Propan',
+    fixed: 1.02,
   },
   {
-    Birim: 'ton',
-    'Enerji Kaynağı': 'LPG',
-    Katsayi: 1.09,
+    unit: 'ton',
+    'Power Supply': 'LPG',
+    fixed: 1.09,
   },
   {
-    Birim: 'm3',
-    'Enerji Kaynağı': 'LPG',
-    Katsayi: 2.7,
+    unit: 'm3',
+    'Power Supply': 'LPG',
+    fixed: 2.7,
   },
   {
-    Birim: 'kWh',
-    'Enerji Kaynağı': 'Elektrik',
-    Katsayi: 0.086,
+    unit: 'kWh',
+    'Power Supply': 'Elektrik',
+    fixed: 0.086,
   },
   {
-    Birim: 'kWh',
-    'Enerji Kaynağı': 'Hidrolik',
-    Katsayi: 0.086,
+    unit: 'kWh',
+    'Power Supply': 'Hidrolik',
+    fixed: 0.086,
   },
   {
-    Birim: 'kWh',
-    'Enerji Kaynağı': 'Jeotermal',
-    Katsayi: 0.86,
+    unit: 'kWh',
+    'Power Supply': 'Jeotermal',
+    fixed: 0.86,
   },
 ];
 
+// const years = [];
+// for (let year = 2000; year <= 2030; ++year) {
+//   years.push(year);
+// }
+
+const years = Array.from({ length: 31 }, (_, i) => 2000 + i);
+
+const months = [
+  'Ocak',
+  'Şubat',
+  'Mart',
+  'Nisan',
+  'Mayıs',
+  'Haziran',
+  'Temmuz',
+  'Ağustos',
+  'Eylül',
+  'Ekim',
+  'Kasım',
+  'Aralık',
+];
+
 const EnergyCalculator = () => {
-  const [energyData, setEnergyData] = useState(energyTypes.map((type) => ({ ...type, amount: 0 })));
+  const [energyData, setEnergyData] = useState(energyenerjiTypes.map((enerjiType) => ({ ...enerjiType, amount: 0 })));
 
   const [selectedYear, setSelectedYear] = useState('');
   const [selectedMonth, setSelectedMonth] = useState('');
-  const [totalTEPs, setTotalTEPs] = useState(0);
+  const [totals, setTotals] = useState(0);
   const [selectedData, setSelectedData] = useState([]);
+
   const [isModalOpen, setIsModalOpen] = useState(false);
 
-  const years = [];
-  for (let year = 2000; year <= 2030; ++year) {
-    years.push(year);
-  }
+  const handleSaveModalData = () => {
+    setIsModalOpen(true);
+  };
 
-  const months = [
-    'Ocak',
-    'Şubat',
-    'Mart',
-    'Nisan',
-    'Mayıs',
-    'Haziran',
-    'Temmuz',
-    'Ağustos',
-    'Eylül',
-    'Ekim',
-    'Kasım',
-    'Aralık',
-  ];
-
-  // Local Storage'dan verileri al
-  useEffect(() => {
-    const storedSelectedData = JSON.parse(localStorage.getItem('selectedData'));
-
-    if (storedSelectedData) {
-      setSelectedData(storedSelectedData);
-    }
-  }, []);
-
-  // Miktar değiştiğinde hesaplamaları yap
-  useEffect(() => {
-    const newTotalTEP = energyData.reduce((total, type) => total + type.amount * type[Katsayi], 0);
-    setTotalTEPs(newTotalTEP);
-
-    // Local Storage'a verileri kaydet
-    localStorage.setItem('selectedData', JSON.stringify(selectedData));
-  }, [energyData, selectedData]);
+  const handleCloseModalData = () => {
+    setIsModalOpen(false);
+  };
 
   const handleYearChange = (e) => {
     setSelectedYear(e.target.value);
@@ -262,19 +255,29 @@ const EnergyCalculator = () => {
     const newData = {
       year: selectedYear,
       month: selectedMonth,
-      totalTEPs: totalTEPs,
+      total: totals,
     };
     setSelectedData([...selectedData, newData]);
     setIsModalOpen(false);
   };
 
-  const handleSaveModalData = () => {
-    setIsModalOpen(true);
-  };
+  // Local Storage'dan verileri al
+  useEffect(() => {
+    const storedSelectedData = JSON.parse(localStorage.getItem('selectedData'));
 
-  const handleCloseModalData = () => {
-    setIsModalOpen(false);
-  };
+    if (storedSelectedData) {
+      setSelectedData(storedSelectedData);
+    }
+  }, []);
+
+  // Miktar değiştiğinde hesaplamaları yap
+  useEffect(() => {
+    const newTotals = energyData.reduce((total, enerjiType) => total + enerjiType.amount * enerjiType.fixed, 0);
+    setTotals(newTotals);
+
+    // Local Storage'a verileri kaydet
+    localStorage.setItem('selectedData', JSON.stringify(selectedData));
+  }, [energyData, selectedData]);
 
   const handleAmountChange = (index, amount) => {
     const updatedEnergyData = [...energyData];
@@ -282,7 +285,7 @@ const EnergyCalculator = () => {
     setEnergyData(updatedEnergyData);
   };
 
-  const totalTEP = energyData.reduce((total, type) => total + type.amount * type[Katsayi], 0);
+  const totalTEP = energyData.reduce((total, enerjiType) => total + enerjiType.amount * enerjiType.fixed, 0);
 
   return (
     <div className="w-full flex flex-col justify-center items-center">
@@ -297,21 +300,21 @@ const EnergyCalculator = () => {
             </tr>
           </thead>
           <tbody>
-            {energyData.map((type, index) => (
+            {energyData.map((enerjiType, index) => (
               <tr key={index} className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
-                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{type['Enerji Kaynağı']}</td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{enerjiType['Power Supply']}</td>
                 <td className="px-6 py-3">
                   <input
-                    type="number"
-                    value={type.amount}
-                    onChange={e => handleAmountChange(index, e.target.value)}
+                    enerjiType="number"
+                    value={enerjiType.amount}
+                    onChange={(e) => handleAmountChange(index, e.target.value)}
                     className="bg-gray-50 w-6/8 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="0"
                     min={0}
                   />
                 </td>
-                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{`(${type[Birim]})`}</td>
-                <td className="px-6 py-3 text-center">{(type.amount * type[Katsayi]).toFixed(3)}</td>
+                <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">{`(${enerjiType.unit})`}</td>
+                <td className="px-6 py-3 text-center">{(enerjiType.amount * enerjiType.fixed).toFixed(3)}</td>
               </tr>
             ))}
           </tbody>
@@ -349,7 +352,7 @@ const EnergyCalculator = () => {
                     Ay: <strong>{data.month}</strong>
                   </td>
                   <td className="px-6 py-4">
-                    Toplam TEP: <strong>{data.totalTEPs.toFixed(2)}</strong>
+                    Toplam TEP: <strong>{data.total.toFixed(2)}</strong>
                   </td>
                 </tr>
               ))}
@@ -375,7 +378,7 @@ const EnergyCalculator = () => {
                   </h3>
                   <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm h-8 w-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="timeline-modal" onClick={handleCloseModalData}>
                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                      <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
                     <span className="sr-only">Close modal</span>
                   </button>
@@ -385,14 +388,7 @@ const EnergyCalculator = () => {
                   <ol className="relative border-s border-gray-200 dark:border-gray-600 ms-3.5 mb-4 md:mb-5">
                     <li className="mb-10 ms-8">
                       <span className="absolute flex items-center justify-center w-6 h-6 bg-gray-100 rounded-full -start-3.5 ring-8 ring-white dark:ring-gray-700 dark:bg-gray-600">
-                        <svg
-                          className="w-2.5 h-2.5 text-gray-500 dark:text-gray-400"
-                          aria-hidden="true"
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none" viewBox="0 0 20 20"
-                        >
-                          <path fill="currentColor" d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z"/>
-                        </svg>
+                        svg
                       </span>
                       <h3 className="mb-3 ml-8 text-lg font-semibold text-gray-900 dark:text-white">Yılı Seçin</h3>
                       <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={selectedYear} onChange={handleYearChange} required>
@@ -406,10 +402,10 @@ const EnergyCalculator = () => {
                     </li>
                     <li className="ms-8">
                       <span className="absolute flex items-center justify-center w-6 h-6 bg-gray-100 rounded-full -start-3.5 ring-8 ring-white dark:ring-gray-700 dark:bg-gray-600">
-                        <svg className="w-2.5 h-2.5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"><path fill="currentColor" d="M6 1a1 1 0 0 0-2 0h2ZM4 4a1 1 0 0 0 2 0H4Zm7-3a1 1 0 1 0-2 0h2ZM9 4a1 1 0 1 0 2 0H9Zm7-3a1 1 0 1 0-2 0h2Zm-2 3a1 1 0 1 0 2 0h-2ZM1 6a1 1 0 0 0 0 2V6Zm18 2a1 1 0 1 0 0-2v2ZM5 11v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 11v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM10 15v-1H9v1h1Zm0 .01H9v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 15v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM15 11v-1h-1v1h1Zm0 .01h-1v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM5 15v-1H4v1h1Zm0 .01H4v1h1v-1Zm.01 0v1h1v-1h-1Zm0-.01h1v-1h-1v1ZM2 4h16V2H2v2Zm16 0h2a2 2 0 0 0-2-2v2Zm0 0v14h2V4h-2Zm0 14v2a2 2 0 0 0 2-2h-2Zm0 0H2v2h16v-2ZM2 18H0a2 2 0 0 0 2 2v-2Zm0 0V4H0v14h2ZM2 4V2a2 2 0 0 0-2 2h2Zm2-3v3h2V1H4Zm5 0v3h2V1H9Zm5 0v3h2V1h-2ZM1 8h18V6H1v2Zm3 3v.01h2V11H4Zm1 1.01h.01v-2H5v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H5v2h.01v-2ZM9 11v.01h2V11H9Zm1 1.01h.01v-2H10v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM9 15v.01h2V15H9Zm1 1.01h.01v-2H10v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H10v2h.01v-2ZM14 15v.01h2V15h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM14 11v.01h2V11h-2Zm1 1.01h.01v-2H15v2Zm1.01-1V11h-2v.01h2Zm-1-1.01H15v2h.01v-2ZM4 15v.01h2V15H4Zm1 1.01h.01v-2H5v2Zm1.01-1V15h-2v.01h2Zm-1-1.01H5v2h.01v-2Z" /></svg>
+                        svg
                       </span>
                       <h3 className="mb-3 ml-8 text-lg font-semibold text-gray-900 dark:text-white">Ayı Seçin</h3>
-                      <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={selectedMonth} onChange={handleMonthChange} required={true}>
+                      <select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" value={selectedMonth} onChange={handleMonthChange} required>
                         <option value="">Seçiniz</option>
                         {months.map((month, index) => (
                           <option key={index} value={month}>
--- src/pages/TepHesapla.jsx
+++ src/pages/TepHesapla.jsx
@@ -1,17 +1,14 @@
+import React from 'react';
 import EnergyCalculator from '../components/EnergyCalculator';
 
-import React from 'react';
-
-const TepHesapla = () => {
-    return (
-        <div className="w-full flex flex-col justify-center items-center">
-            <div className="w-full max-w-6xl flex flex-col items-center justify-center mb-6">
-                <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-[38px] dark:text-white">Enerji <span class="text-blue-600 dark:text-blue-500">TEP</span> Hesaplayıcı</h1>
-                <p class="text-md font-normal text-gray-500 lg:text-md dark:text-gray-400 text-justify">Ton eşdeğer petrol (TEP), bir ton ham petrolün yanmasıyla elde edilen enerjiye karşılık gelen enerji birimidir. Yaklaşık olarak 10 milyon kcal enerjiyi ifade eder. Tüm enerji kaynaklarının TEP'e dönüşüm katsayıları 25 Ekim 2008 tarihli Enerji Kaynaklarının ve Enerjinin Kullanımında Verimliliğin Artırılmasına Dair Yönetmelik ile belirlenmiştir. Yapılan hesaplarda bu değerler dikkate alınmalıdır</p>
-            </div>
-            <EnergyCalculator />
-        </div>
-    );
-}
+const TepHesapla = () => (
+  <div className="w-full flex flex-col justify-center items-center">
+    <div className="w-full max-w-6xl flex flex-col items-center justify-center mb-6">
+      <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl lg:text-[38px] dark:text-white">Enerji <span className="text-blue-600 dark:text-blue-500">TEP</span> Hesaplayıcı</h1>
+      <p className="text-md font-normal text-gray-500 lg:text-md dark:text-gray-400 text-justify">Ton eşdeğer petrol (TEP), bir ton ham petrolün yanmasıyla elde edilen enerjiye karşılık gelen enerji birimidir. Yaklaşık olarak 10 milyon kcal enerjiyi ifade eder. Tüm enerji kaynaklarının TEP&apos;e dönüşüm katsayıları 25 Ekim 2008 tarihli Enerji Kaynaklarının ve Enerjinin Kullanımında Verimliliğin Artırılmasına Dair Yönetmelik ile belirlenmiştir. Yapılan hesaplarda bu değerler dikkate alınmalıdır</p>
+    </div>
+    <EnergyCalculator />
+  </div>
+);
 
 export default TepHesapla;
